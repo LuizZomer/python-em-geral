5184f899b25fe894f4cade359e9f31771f89846d
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-05-18 10:05:15 -0300

    adição sobre lista

---
+++ listas/comandos.py
@@ -0,0 +1,14 @@
+lista = [3,2,'Luiz',1]
+
+# lista.remove(3) #Remove o item desejado pelo nome
+lista.remove('Luiz')
+
+print(lista)
+
+outra = list(range(6,12))
+
+print(outra)
+
+lista.sort() #lista.sort(reverse = True) para ordenar a lista de tras para frente
+
+print(lista)
