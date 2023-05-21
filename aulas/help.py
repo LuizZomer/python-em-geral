a7100574ec35097386bbef1415579000cddcad9b
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-05-20 21:13:12 -0300

    Create help.py

---
+++ aulas/help.py
@@ -0,0 +1,10 @@
+def soma(a,b):
+    """
+    A definição soma pega o argumento 'a' e 'b' e soma, printando na tela.    
+    """
+    s = a+b
+    print(s)
+
+soma(3,4)
+
+help(soma) #A docstrigs(as 3 aspas duplas) cria um manual da sua função, que pode ser acessado usando a função help
