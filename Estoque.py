cb1a1b83947958738130bc37772fd56a0a1b75d0
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-05-22 23:55:04 -0300

    completo

--- Estoque.py
+++ Estoque.py
@@ -10,23 +10,28 @@ produto = {}
 estoque = []
 
 while True:
+    cont = 0
     titulo('estoque')
-    print('1 - visualizar estoque\n2 - cadastrar produtos\n3 - fechar programa')
+    print('1 - visualizar estoque\n2 - cadastrar produtos\n3 - apagar produtos\n4 = Sair do programa')
     opcaotxt = input('Escolha uma opção: ')
     try:
         opcao = int(opcaotxt)
     except ValueError:
         print('Valor invalido')
-        continue
+        sleep(1)
+        continue    
     match opcao:
         case 1:
             titulo('Visualização do estoque')
             if len(estoque) == 0:
                 print('O estoque está vazio')
+                sleep(1)
             else:
-                print(f'{"Nome":<10}  |  {"preço":>1}')
+                print(f'{"nº":<1} |   {"Nome":^5}  |  {"preço":>1}')
                 for p in estoque:
-                    print(f'{p["nome"]:<10} {p["preco"]:>10}')
+                    print(f'{cont:<1}  {p["nome"]:^10} {p["preco"]:>5}')
+                    cont+=1
+                    sleep(1)
         case 2:
             titulo('Cadastro de produtos')
             produto['nome'] = input('Nome do produto: ')
@@ -34,8 +39,21 @@ while True:
             print(f'O produto {produto["nome"]} foi cadastrado com sucesso.')
             estoque.append(produto.copy())
             produto.clear()
+            sleep(1)
         case 3:
+            deletartxt = input('Escolha o numero do produto para excluir: ')
+            try:
+                deletar = int(deletartxt)
+            except ValueError:
+                print('Valor incorreto')
+            if deletar > len(estoque)-1:
+                print('Numero inxistente')
+            else:
+                estoque.pop(deletar)
+                sleep(1)
+        case 4:
             print('Saindo do programa...')
             break
         case _:
             print('Opção invalida')
+            sleep(1)
