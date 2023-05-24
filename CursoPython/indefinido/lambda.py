88856ff0bcc34320afe7dfa83785893d4e173830
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-05-24 10:35:09 -0300

    função lambda

---
+++ CursoPython/indefinido/lambda.py
@@ -0,0 +1,24 @@
+# l = [1,2,3,4,5]
+
+l = [
+    {'nome':'Luiz','sobrenome':'Zomer'},
+    {'nome':'Maria','sobrenome':'Silva'},
+    {'nome':'Arthur','sobrenome':'Silveira'},
+    {'nome':'Eduardo','sobrenome':'Garcia'}
+]
+
+# def ordena(item): Ordena o dicionario pela chave nome
+#     return item['nome']
+
+# def ordena(item): Ordena o dicionario pelo sobrenome
+#     return item['sobrenome']
+
+
+# l.sort(key=lambda item: item['nome']) Realmente modifica a lista
+l1 = sorted(l, key=lambda item: item['nome']) #Faz uma copia rasa da lista
+
+for pessoa in l1:
+    print(pessoa)
+
+
+
